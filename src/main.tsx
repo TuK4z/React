eea9cc27eee98f51d1bf9195fbd4219260b83fb3
aydek <kesa0915@hotmail.com>
2025-01-13 17:30:18 +0000

    Redone notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { NotificationProvider } from './components/Notifications/NotificationContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <NotificationProvider>
     <HashRouter>
-      <App />
+        <App />
     </HashRouter>
-  </NotificationProvider>
 );
